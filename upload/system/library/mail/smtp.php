ade25a1c629b85b930da312c3bd73c83fdd2a203
dhivyabharathi <pdivyabharathi21@gmail.com>
2021-06-16 13:43:56 +0530

    initial commit

---
+++ upload/system/library/mail/smtp.php
@@ -0,0 +1,14 @@
+<?php
+namespace Mail;
+class Smtp {
+	public $smtp_hostname;
+	public $smtp_username;
+	public $smtp_password;
+	public $smtp_port = 25;
+	public $smtp_timeout = 5;
+	public $verp = false;
+
+	public function send() {
+		
+	}
+}
